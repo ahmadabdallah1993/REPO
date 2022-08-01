c46a5ebd77780d69c490946fcbc307d8b084e491
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-01 18:12:01 +0300

    js practice

---
+++ app.js
@@ -0,0 +1,146 @@
+console.log('class py');
+/*
+//-----------------------------------
+
+//pop up boxes:
+
+
+// 1. alert
+alert("welcome to our website");
+
+
+// 2. prompt
+//prompt("enter your name");
+
+var userName = prompt("enter your name");
+console.log(userName);
+
+
+// 3. confirm
+//confirm("Are You Sure?");
+
+var x = confirm("Are You Sure?");
+console.log(x);
+
+
+//---------------------------------
+
+
+// conditions:
+
+var userAge = prompt("enter your age");
+
+
+if(userAge < 10) {
+    alert("you have a school tomorrow !!");
+} else {
+    alert("have fun !!");
+}
+
+
+
+// ternary if
+
+
+userAge < 10 ? alert("you have a school tomorrow !!") : alert("have fun !!");
+
+
+
+//---------------------------------
+
+
+// naming convision
+
+var userName = "user";
+
+var x = 10;
+var X = 15; // they are not the same
+
+
+
+//---------------------------------
+
+
+// arrays:
+
+var names = ["walaa", "ahmad"];
+names.push("ah");
+//names[2] = "rz";
+names.unshift("mh");
+console.log(names[2]);
+
+console.log(names);
+
+
+//---------------------------------
+
+
+
+//loops:
+
+for(var i = 0; i < names.length; i ++) {
+
+    console.log(i);
+    console.log(names[i]);
+
+    if(names[i] == "ahmad") {
+        console.log("found it");
+        //break;
+        i = names.length;  //it mean after you found it break in another way
+    }
+}
+
+
+*/
+
+
+//---------------------------------
+
+
+//function:
+
+function greeting() {
+    alert("welcome home");
+    console.log("welcome");
+}
+
+greeting();
+
+
+
+
+
+function division(x, y) {
+
+    return x / y;
+}
+
+
+var result = division(9,3);
+console.log(result);
+
+
+
+//---------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
